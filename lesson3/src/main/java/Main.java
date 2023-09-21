5578c662de8c0f4d9eb715aaf3137e50b14bd511
Daria <procionestrisce@yandex.ru>
2023-09-21 19:52:14 +0300

    Задание 7

---
+++ lesson3/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        printText("Hello world");
+
+    }
+
+    public static void printText(String text){
+        System.out.println(text);
+    }
+}
